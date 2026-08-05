f896d0986e8dfcfd0b2e608b1ed3503872491a21
akglozan <100809979+akglozan@users.noreply.github.com>
2026-08-05 13:56:01 +0200

    Create Forwarding_Unit.vhd

---
+++ Forwarding_Unit.vhd
@@ -0,0 +1,49 @@
+library IEEE;
+use IEEE.STD_LOGIC_1164.all;
+use IEEE.NUMERIC_STD.all;
+
+entity Forwarding_Unit is
+
+	port(
+	
+		ex_rs1_addr		: in std_logic_vector(4 downto 0);
+		ex_rs2_addr		: in std_logic_vector(4 downto 0);
+		mem_rd_addr		: in std_logic_vector(4 downto 0);
+		mem_reg_write	: in std_logic;
+		wb_rd_addr		: in std_logic_vector(4 downto 0);
+		wb_reg_write	: in std_logic;
+		
+		forward_a		: out std_logic_vector(1 downto 0);
+		forward_b		: out std_logic_vector(1 downto 0)
+	);
+
+end entity;
+
+
+architecture Behavioral of Forwarding_Unit is
+
+	
+begin
+
+process(all)
+begin
+	if mem_reg_write = '1' and mem_rd_addr /= "00000" and mem_rd_addr = ex_rs1_addr then
+		forward_a <= "10";
+	elsif wb_reg_write = '1' and wb_rd_addr /= "00000" and wb_rd_addr = ex_rs1_addr then
+		forward_a <= "01";
+	else
+		forward_a <= "00";
+	end if;
+	
+	if mem_reg_write = '1' and mem_rd_addr /= "00000" and mem_rd_addr = ex_rs2_addr then
+		forward_b <= "10";
+	elsif wb_reg_write = '1' and wb_rd_addr /= "00000" and wb_rd_addr = ex_rs2_addr then
+		forward_b <= "01";
+	else
+		forward_b <= "00";
+	end if;
+
+end process;
+
+
+end architecture;
