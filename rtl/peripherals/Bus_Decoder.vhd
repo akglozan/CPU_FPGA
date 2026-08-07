b72abee872e8640521dd2a21feb388f114164686
akglozan <100809979+akglozan@users.noreply.github.com>
2026-08-07 19:05:30 +0200

    Update Bus_Decoder.vhd

--- rtl/peripherals/Bus_Decoder.vhd
+++ rtl/peripherals/Bus_Decoder.vhd
@@ -1,1 +1,18 @@
-s
+library IEEE;
+use IEEE.STD_LOGIC_1164.all;
+use IEEE.NUMERIC_STD.all;
+
+entity Bus_Decoder is
+    
+    port (
+        clk      : in  std_logic;
+        rst      : in  std_logic
+        
+    );
+end entity Bus_Decoder;
+
+architecture of Bus_Decoder is
+
+begin
+
+end architecture;
