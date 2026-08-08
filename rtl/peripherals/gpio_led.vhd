7235439caa992fb180e7d0d7dd11d1293faf7b2e
akglozan <100809979+akglozan@users.noreply.github.com>
2026-08-08 02:26:05 +0200

    created gpio_led file

---
+++ rtl/peripherals/gpio_led.vhd
@@ -0,0 +1,19 @@
+library IEEE;
+use IEEE.STD_LOGIC_1164.all;
+use IEEE.NUMERIC_STD.all;
+
+entity gpio_led is
+
+	port(
+		clk : in std_logic
+	);
+end entity;
+
+
+architecture Behavioral of gpio_led is
+	
+begin 
+
+
+
+end architecture Behavioral;
